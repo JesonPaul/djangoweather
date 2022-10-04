97568c6e10ed0cc90aa8260938a650b0f05500c8
Jeson <jesonpl2006@gmail.com>
2022-10-04 11:27:22 +0530

    update

--- lookup/views.py
+++ lookup/views.py
@@ -11,7 +11,32 @@ def home(request):
 	except Exception as e:
 		api = "Error..."
 
-	return render (request, 'home.html', {'api': api})
+	if api[0]['Category']['Name'] == "Good":
+		category_description = "(0 -50) Air quality is considered satisfactory, and air pollution poses little or no risk"
+		category_color = "good"
+
+	elif api[0]['Category']['Name'] == "Moderate": 
+		category_description = "(51 -100) If you are unusually sensitive to ozone, consider reducing your activity level or shorten the amount of time you are active outdoors."
+		category_color = "moderate"
+
+	elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+		category_description = "(101 -150) Although general public is not likely to be affected at thi AQI range, people with lung disease, older adults and children are at a greater risk from exposure to ozone, whereas person with heart and lung disease are at greater risk from the presence of particles in air."
+		category_color = "Unhealthy for Sensitive Groups"
+
+	elif api[0]['Category']['Name'] == "Unhealthy":
+		category_description = "(151 -200) Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."
+		category_color = "unhealthy"
+
+	elif api[0]['Category']['Name'] == "Very unhealthy":
+		category_description = "(201 -300) Health Alert: everyone may experience more serious health effects."
+		category_color = "veryunhealthy"
+
+	elif api[0]['Category']['Name'] == "Hazardous":
+		category_description = "(301 -500) Health warnings of emergency conditions. The entire population is more likely to be affected. "
+		category_color = "hazardous"
+
+
+	return render (request, 'home.html', {'api': api, 'category_description': category_description, 'category_color': category_color})
 
 def about(request):
 	return render (request, 'about.html', {})
